77615f4a04538147759828fb48e0216bab37cce7
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2021-04-09 21:43:05 +0200

    Fixes #4 and fixes #5.

--- pages/api/json/[...slug].js
+++ pages/api/json/[...slug].js
@@ -7,45 +7,63 @@ export default async function handler(req, res) {
   const village = getVillage(slug[0]);
   if (!village) res.status(400);
 
-  const icsFeed = await getIcsFeed(village.id, village.region);
-  const originalEvents = ical.sync.parseICS(icsFeed);
-
   let fixedEvents = [];
+  for (let i = 0; i < village.ids.length; i++) {
+    const icsFeed = await getIcsFeed(village.ids[i].id, village.region);
+    const originalEvents = ical.sync.parseICS(icsFeed);
 
-  for (const event of Object.values(originalEvents)) {
-    if (event.type === 'VEVENT') {
-      let tmpEvent = JSON.parse(JSON.stringify(event));
-      tmpEvent.categories = 'Entsorgung';
-      if (tmpEvent.location.includes('Schadstoffmobil')) {
-        const tmpVillage = tmpEvent.location.split(':')[0].trim();
-        const tmpLocation = tmpEvent.description.split('\n')[0].split(':')[1].trim();
-        tmpEvent.summary = tmpEvent.summary.trim();
-        tmpEvent.location = tmpLocation + ', ' + tmpVillage;
-        tmpEvent.description = tmpEvent.description.split('\n')[0].trim();
-      } else {
-        tmpEvent.description = tmpEvent.summary + '\n' + tmpEvent.location;
-        tmpEvent.location = village.name;
-        if (tmpEvent.summary.includes('Restmüll')) tmpEvent.summary = 'Restmüll';
-        if (tmpEvent.summary.includes('Papier')) tmpEvent.summary = 'Altpapier';
-        if (tmpEvent.summary.includes('gelb')) tmpEvent.summary = 'Gelber Sack';
+    for (const event of Object.values(originalEvents)) {
+      if (event.type === 'VEVENT') {
+        let tmpEvent = JSON.parse(JSON.stringify(event));
+        tmpEvent.categories = ['Waste disposal'];
+        if (tmpEvent.location.includes('Schadstoffmobil')) {
+          const tmpVillage = tmpEvent.location.split(':')[0].trim();
+          const tmpLocation = tmpEvent.description.split('\n')[0].split(':')[1].trim();
+          tmpEvent.summary = tmpEvent.summary.trim();
+          tmpEvent.location = tmpLocation + ', ' + tmpVillage;
+          tmpEvent.description = tmpEvent.description.split('\n')[0].trim();
+          tmpEvent.scope = 'municipality';
+          tmpEvent.categories.push('Hazardous waste');
+        } else {
+          tmpEvent.description = tmpEvent.summary + '\n' + tmpEvent.location;
+          if (village.ids[i].remark)
+            tmpEvent.description += '\n' + village.name + ' ' + village.ids[i].remark;
+          if (village.remark) tmpEvent.description += '\n' + village.name + ' ' + village.remark;
+          tmpEvent.location = village.name;
+          if (tmpEvent.summary.includes('Restmüll')) {
+            tmpEvent.summary = 'Restmüll';
+            tmpEvent.categories.push('Residual waste');
+          }
+          if (tmpEvent.summary.includes('Papier')) {
+            tmpEvent.summary = 'Altpapier';
+            tmpEvent.categories.push('Waste paper');
+          }
+          if (tmpEvent.summary.includes('gelb')) {
+            tmpEvent.summary = 'Gelber Sack';
+            tmpEvent.categories.push('Packaging waste');
+          }
+          tmpEvent.scope = 'community';
+        }
+
+        const newEvent = {
+          id: tmpEvent.uid,
+          summary: tmpEvent.summary,
+          description: tmpEvent.description,
+          location: tmpEvent.location,
+          start: tmpEvent.start,
+          end: tmpEvent.end,
+          updated: tmpEvent.dtstamp,
+          categories: tmpEvent.categories,
+          organizer: {
+            email: 'info@vevg-karlsburg.de',
+            displayName: 'VEVG Karlsburg',
+          },
+          visibility: tmpEvent.class.toLowerCase(),
+          scope: tmpEvent.scope,
+        };
+
+        fixedEvents.push(newEvent);
       }
-
-      const newEvent = {
-        id: tmpEvent.uid,
-        summary: tmpEvent.summary,
-        description: tmpEvent.description,
-        location: tmpEvent.location,
-        start: tmpEvent.start,
-        end: tmpEvent.end,
-        updated: tmpEvent.dtstamp,
-        organizer: {
-          email: 'info@vevg-karlsburg.de',
-          displayName: 'VEVG Karlsburg',
-        },
-        visibility: tmpEvent.class.toLowerCase(),
-      };
-
-      fixedEvents.push(newEvent);
     }
   }
 
